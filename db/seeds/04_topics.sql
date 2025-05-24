e4581b9b55bbe52f5a02a6f8456db93ec7a2e072
VktYe <v.yehorenko@gmail.com>
2025-05-23 23:12:09 -0300

    Insert fake topics data

---
+++ db/seeds/04_topics.sql
@@ -0,0 +1,6 @@
+INSERT INTO topics (name) VALUES ('JavaScript');
+INSERT INTO topics (name) VALUES ('CSS');
+INSERT INTO topics (name) VALUES ('Databases');
+INSERT INTO topics (name) VALUES ('Node.js');
+INSERT INTO topics (name) VALUES ('React');
+INSERT INTO topics (name) VALUES ('Web Development');

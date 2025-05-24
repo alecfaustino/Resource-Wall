fbc79120d3e5c38f2298fa59ea214bd8c43f5e87
VktYe <v.yehorenko@gmail.com>
2025-05-23 22:07:10 -0300

    Update password for hashed password

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,32 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+-- hashed password is "password"
+INSERT INTO users (name, email, password)
+VALUES ('Alice Johnson', 'alice@example.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+
+INSERT INTO users (name, email, password)
+VALUES ('Bob Smith', 'bob@example.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+
+INSERT INTO users (name, email, password)
+VALUES ('Charlie Lee', 'charlie@example.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+
+INSERT INTO users (name, email, password)
+VALUES ('Diana King', 'diana@example.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+
+INSERT INTO users (name, email, password)
+VALUES ('Ethan Fox', 'ethan@example.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+
+INSERT INTO users (name, email, password)
+VALUES ('Fiona Green', 'fiona@example.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+
+INSERT INTO users (name, email, password)
+VALUES ('George Blake', 'george@example.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+
+INSERT INTO users (name, email, password)
+VALUES ('Hannah Rose', 'hannah@example.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+
+INSERT INTO users (name, email, password)
+VALUES ('Ivan Clark', 'ivan@example.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+
+INSERT INTO users (name, email, password)
+VALUES ('Julia Wells', 'julia@example.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+

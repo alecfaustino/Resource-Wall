8282ae2d297e49888baf43d345a7d3d91291d0f7
VktYe <v.yehorenko@gmail.com>
2025-05-24 00:32:46 -0300

    Insert fake likes data

---
+++ db/seeds/06_likes.sql
@@ -0,0 +1,7 @@
+-- Likes by various users on resources
+INSERT INTO resource_likes (user_id, resource_id) VALUES (1, 1);
+INSERT INTO resource_likes (user_id, resource_id) VALUES (2, 1);
+INSERT INTO resource_likes (user_id, resource_id) VALUES (3, 2);
+INSERT INTO resource_likes (user_id, resource_id) VALUES (1, 3);
+INSERT INTO resource_likes (user_id, resource_id) VALUES (4, 3);
+INSERT INTO resource_likes (user_id, resource_id) VALUES (5, 2);

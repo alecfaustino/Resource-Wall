455af29e3901c977b960468bcfd51c62644317ef
VktYe <v.yehorenko@gmail.com>
2025-05-25 16:15:41 -0300

    Add topic to display

--- routes/resources.js
+++ routes/resources.js
@@ -12,13 +12,15 @@ router.get('/', (req, res) => {
 
   // Temporary hardcoded data
   const userResources = [
-    { id: 1, title: 'Titile', description: 'Description', url: 'Url here', author: 'Vika' },
-    { id: 2, title: 'Titile', description: 'Description', url: 'Url here', author: 'Vika' }
+    { id: 1, title: 'Titile', description: 'Description', url: 'Url here', topic: 'topic', author: 'Vika' },
+    { id: 2, title: 'Titile', description: 'Description', url: 'Url here',
+    topic: 'topic',  author: 'Vika' }
   ];
 
   const likedResources = [
-    { id: 3, title: 'Titile', description: 'Description', url: 'Url here', author: 'Vika' },
-    { id: 4, title: 'Titile', description: 'Description', url: 'Url here', author: 'Vika' }
+    { id: 3, title: 'Titile', description: 'Description', url: 'Url here',
+    topic: 'topic',  author: 'Vika' },
+    { id: 4, title: 'Titile', description: 'Description', url: 'Url here', topic: 'topic', author: 'Vika' }
   ];
 
   res.render('resources', { userResources, likedResources });

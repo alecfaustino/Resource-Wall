651c62121ebaa58ea151a925b95f359ae0568a2e
Alec Faustino <alecfaustino@outlook.com>
2025-05-28 11:00:57 -0600

    change the profile page to display the user info from the db

--- routes/profile.js
+++ routes/profile.js
@@ -1,14 +1,33 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const db = require("../db/connection");
 
-
-router.get('/', (req, res) => {
+router.get("/", async (req, res) => {
   // Hard-coded user data for now
-  const user = {
-    name: 'Vika',
-    email: 'vika@example.com'
-  };
-  res.render('profile', {user});
+
+  // TODO: remove the || 1 after testing
+  const userId = req.session.user_id || 1;
+
+  const userInfoQuery = `
+    SELECT *
+    FROM users
+    WHERE id = $1
+  `;
+
+  try {
+    const userInfoResult = await db.query(userInfoQuery, [userId]);
+
+    if (userInfoResult.rows.length === 0) {
+      return res.status(404).send("User not found!");
+    }
+    const user = userInfoResult.rows[0];
+    res.render("profile", { user });
+  } catch (error) {
+    console.error("Error loading user data");
+    res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
 });
 
 module.exports = router;

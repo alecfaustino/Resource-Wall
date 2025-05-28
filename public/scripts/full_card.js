6344f8b3d667248ee1df8380b77870edb805d8b0
Alec Faustino <alecfaustino@outlook.com>
2025-05-27 21:31:11 -0600

    add delete handler

--- public/scripts/full_card.js
+++ public/scripts/full_card.js
@@ -4,4 +4,24 @@ $(document).ready(() => {
     console.log("return-btn clicked");
     window.location.href = "/";
   });
+
+  $("#delete-btn").on("click", function () {
+    const resourceId = $(this).data("resource-id");
+
+    if (!resourceId) {
+      console.error("Resource ID not found");
+      return;
+    }
+
+    $.ajax({
+      url: `/api/resources/${resourceId}`,
+      type: "DELETE",
+      success: () => {
+        window.location.href = "/";
+      },
+      error: () => {
+        console.error("Failed to Delete Resource");
+      },
+    });
+  });
 });

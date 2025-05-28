743b9482da5052849c8c15df08d7f0121929291e
Alec Faustino <alecfaustino@outlook.com>
2025-05-27 22:19:25 -0600

    the edit and delete will work if the user owns the resource

--- public/scripts/full_card.js
+++ public/scripts/full_card.js
@@ -24,4 +24,75 @@ $(document).ready(() => {
       },
     });
   });
+
+  // edit mode not on by default
+  let isEditing = false;
+
+  // this edit function currently doesn't allow the topic to be editted.
+  // the url description is also not edittable
+  $("#edit-btn").on("click", function () {
+    const resourceId = $(this).data("resource-id");
+
+    // if not editting,
+    if (!isEditing) {
+      // store the current values
+      const title = $(".resource-title").text().trim();
+      const description = $(".resource-description").text().trim();
+      const linkA = $(".resource-link");
+      const linkName = linkA.data("link-name");
+      const linkUrl = linkA.attr("href");
+
+      // make the text change into input field
+      $(".resource-title").replaceWith(
+        `
+        <label>Resource Title</label>
+        <input type='text' class='resource-title-edit' value="${title}">`
+      );
+      $(".resource-description").replaceWith(
+        `
+        <label>Resource Description</label>
+        <textarea class="resource-description-edit">${description}</textarea>`
+      );
+      linkA.replaceWith(`
+        <div class="link-edit">
+          <label>Link Name</label>
+          <input type="text" class="link-name-edit" value="${linkName}">
+          <input type="text" class="link-url-edit" value="${linkUrl}">
+        </div>
+      `);
+
+      // the edit button text change to Save
+      $(this).text("Save");
+      // edit mode
+      isEditing = true;
+    } else {
+      // needs to be an else because if you're already editting, then the isEditting is true and we'll send an ajax request to save.
+      // Saving the changes
+      const updatedtitle = $(".resource-title-edit").val().trim();
+      const updatedDescription = $(".resource-description-edit").val().trim();
+      const updatedLinkName = $(".link-name-edit").val().trim();
+      const updatedLinkUrl = $(".link-url-edit").val().trim();
+
+      $.ajax({
+        url: `/api/resources/${resourceId}`,
+        type: "PATCH",
+        data: {
+          title: updatedtitle,
+          description: updatedDescription,
+          link: {
+            name: updatedLinkName,
+            url: updatedLinkUrl,
+          },
+        },
+        success: () => {
+          //reload the page
+          console.log("patch successful");
+          location.reload();
+        },
+        error: () => {
+          console.error("Failed to update resource");
+        },
+      });
+    }
+  });
 });
--- routes/resources-api.js
+++ routes/resources-api.js
@@ -1,15 +1,14 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const db = require('../db/connection')
+const db = require("../db/connection");
 // we can create a queries file later in the db folder to clean up these routes.
 
 // get all the resources
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   // Order by Newest at the top
   const search = req.query.search;
   const queryParams = [];
-  let queryString =
-  `
+  let queryString = `
     SELECT
       resources.id,
       resources.title,
@@ -36,91 +35,95 @@ router.get('/', async (req, res) => {
   }
   queryString += ` ORDER BY resources.created_at DESC LIMIT 10;`;
 
-
   try {
     const result = await db.query(queryString, queryParams);
     res.json(result.rows);
   } catch (error) {
     // backend console message
-    console.error('Error fetching resources: ', error);
+    console.error("Error fetching resources: ", error);
     // send a message to the client
     // Server error
     // This will just send JSON for error handling at this point - we can (if we have time) send a better looking error message in the future)
     // will be useful to test with postman, too.
-    res.status(500).json({error: 'Internal Server Error'});
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
 //get a single resource
 //some comments from above apply here
-router.get('/:id', async (req, res) => {
+router.get("/:id", async (req, res) => {
   const resource_id = req.params.id;
 
-  const queryString =
-  `
+  const queryString = `
   SELECT *
   FROM resources
   WHERE id = $1
-  `
+  `;
 
   try {
     const result = await db.query(queryString, [resource_id]);
 
     // if the targetted resource is not found
-    if (result.rows.length === 0) return res.status(404).json({error: 'Resource not found'});
+    if (result.rows.length === 0)
+      return res.status(404).json({ error: "Resource not found" });
     res.json(result.rows);
   } catch (error) {
-    console.error('Error fetching resources: ', error);
-    res.status(500).json({error: 'Internal Server Error'});
+    console.error("Error fetching resources: ", error);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
 // adding a resources
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => {
   // This line makes the user_id equal to whatever we put in the url params (we are hard coding this for this project)
   // TODO: REMOVE || 1 after development -- this is for postman testing
   const userId = req.session.user_id || 1;
 
   // values from the form (front end)
-  const {title, description, link, topic} = req.body;
+  const { title, description, link, topic } = req.body;
 
   // validation if any of these required fields are not filled
   // send error message for testing in postman/seeing in client JSON
-  if (!title || !link || !link.name || !link.url) return res.status(400).json({error: 'Missing required fields'});
+  if (!title || !link || !link.name || !link.url)
+    return res.status(400).json({ error: "Missing required fields" });
 
   // INSERT into the resources table
-  const resourceQueryString =
-  `INSERT INTO resources (author_id, title, description, created_at)
+  const resourceQueryString = `INSERT INTO resources (author_id, title, description, created_at)
   VALUES ($1, $2, $3, NOW())
   RETURNING *;
   `;
 
   // INSERT into the resource_links table
-  const linkQueryString =
-  `INSERT INTO resource_links (resource_id, name, url, description)
+  const linkQueryString = `INSERT INTO resource_links (resource_id, name, url, description)
   VALUES ($1, $2, $3, $4)
   RETURNING *;
   `;
 
   // INSERT into resource_topics table
-  const topicQueryString =
-  ` INSERT INTO resource_topics (resource_id, topic_id)
+  const topicQueryString = ` INSERT INTO resource_topics (resource_id, topic_id)
   VALUES ($1, $2)
   RETURNING *;
-  `
-
+  `;
 
   try {
     // validate if logged in
     if (!userId) {
-      return res.status(403).json({ error: 'Not authorized' });
+      return res.status(403).json({ error: "Not authorized" });
     }
     // insert into resources
-    const resourceQueryValues = [ userId , title, description];
-    const resourceResult = await db.query(resourceQueryString, resourceQueryValues);
+    const resourceQueryValues = [userId, title, description];
+    const resourceResult = await db.query(
+      resourceQueryString,
+      resourceQueryValues
+    );
     const resource = resourceResult.rows[0];
     // insert into resource_links
-    const linkQueryValues = [resource.id, link.name, link.url, link.description];
+    const linkQueryValues = [
+      resource.id,
+      link.name,
+      link.url,
+      link.description,
+    ];
     const linkResult = await db.query(linkQueryString, linkQueryValues);
     const linkResultRow = linkResult.rows[0];
 
@@ -133,35 +136,31 @@ router.post('/', async (req, res) => {
     res.status(201).json({
       resource,
       link: linkResultRow,
-      topic: insertedTopic
-    })
-
-  } catch (error) {
-    console.error('Error creating resource with link: ', error);
-    res.status(500).json({
-      error: 'Internal Server Error'
+      topic: insertedTopic,
     });
-
+  } catch (error) {
+    console.error("Error creating resource with link: ", error);
+    res.status(500).json({
+      error: "Internal Server Error",
+    });
   }
 });
 
 //edit a resource
 //using patch as we'd only be updating a part of the resource
-router.patch('/:id', async (req, res) => {
+router.patch("/:id", async (req, res) => {
   const resource_id = req.params.id;
   // TODO: REMOVE || 1 after development -- this is for postman testing
   const userId = req.session.user_id || 1;
   const { title, description, link } = req.body;
-  const editResourceQueryString =
-  `UPDATE resources
+  const editResourceQueryString = `UPDATE resources
   SET title = $1,
   description = $2
   WHERE id = $3
   RETURNING *;
   `;
 
-  const editResourceLinksQueryString =
-  `UPDATE resource_links
+  const editResourceLinksQueryString = `UPDATE resource_links
   SET name = $1,
   url = $2,
   description = $3
@@ -170,8 +169,7 @@ router.patch('/:id', async (req, res) => {
   `;
 
   // to check if the user owns the resource
-  const resourceOwnerQueryString =
-  ` SELECT *
+  const resourceOwnerQueryString = ` SELECT *
   FROM resources
   WHERE id = $1 AND author_id = $2
   `;
@@ -179,53 +177,68 @@ router.patch('/:id', async (req, res) => {
   const resourceOwnerQueryStringValues = [resource_id, userId];
 
   try {
-
     // validate if logged in
     if (!userId) {
-      return res.status(403).json({ error: 'Not authorized' });
+      return res.status(403).json({ error: "Not authorized" });
     }
-    if (!title || !link || !link.name || !link.url) return res.status(400).json({error: 'Missing required fields'});
+    if (!title || !link || !link.name || !link.url)
+      return res.status(400).json({ error: "Missing required fields" });
 
-    const resourceOwnerQueryResult = await db.query(resourceOwnerQueryString, resourceOwnerQueryStringValues);
+    const resourceOwnerQueryResult = await db.query(
+      resourceOwnerQueryString,
+      resourceOwnerQueryStringValues
+    );
 
     // if the query returns something, then there would be a atleast one row. If 0 come back, the user is not the owner of the resource.
-    if (resourceOwnerQueryResult.rows.length === 0) return res.status(403).json({error: 'Unauthorized access to edit this resource'});
+    if (resourceOwnerQueryResult.rows.length === 0)
+      return res
+        .status(403)
+        .json({ error: "Unauthorized access to edit this resource" });
 
     // if it's successful, proceed to update.
     const editResourceQueryValues = [title, description, resource_id];
-    const updateResourceResult = await db.query(editResourceQueryString, editResourceQueryValues);
+    const updateResourceResult = await db.query(
+      editResourceQueryString,
+      editResourceQueryValues
+    );
 
     // update the link, too.
-    const editResourceLinksQueryValues = [link.name, link.url, link.description, resource_id]
-    const updateLinkResult = await db.query(editResourceLinksQueryString, editResourceLinksQueryValues);
+    const editResourceLinksQueryValues = [
+      link.name,
+      link.url,
+      link.description,
+      resource_id,
+    ];
+    const updateLinkResult = await db.query(
+      editResourceLinksQueryString,
+      editResourceLinksQueryValues
+    );
 
     // if all is successful,
     res.status(200).json({
       resource: updateResourceResult.rows[0],
-      link: updateLinkResult.rows[0]
+      link: updateLinkResult.rows[0],
     });
   } catch (error) {
-    console.error('Error updating resource: ', error);
-    res.status(500).json({ error: 'Internal Server Error'});
+    console.error("Error updating resource: ", error);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
 //delete
 // Same as explained above - sending back JSON for now while no front end.
 // Will redirect when views are created.
-router.delete('/:id', async (req, res) => {
+router.delete("/:id", async (req, res) => {
   const resource_id = req.params.id;
   // TODO: REMOVE || 1 after development -- this is for postman testing
   const userId = req.session.user_id || 1;
 
   // to check if the user owns the resource
-  const resourceOwnerQueryString =
-  ` SELECT *
+  const resourceOwnerQueryString = ` SELECT *
   FROM resources
   WHERE id = $1 AND author_id = $2
   `;
-  const deleteResourceQueryString =
-  `DELETE FROM resources
+  const deleteResourceQueryString = `DELETE FROM resources
   WHERE id = $1
   RETURNING *
   `;
@@ -235,21 +248,28 @@ router.delete('/:id', async (req, res) => {
   try {
     // validate if logged in
     if (!userId) {
-      return res.status(403).json({ error: 'Not authorized' });
+      return res.status(403).json({ error: "Not authorized" });
     }
-    const resourceOwnerQueryResult = await db.query(resourceOwnerQueryString, resourceOwnerQueryValues);
+    const resourceOwnerQueryResult = await db.query(
+      resourceOwnerQueryString,
+      resourceOwnerQueryValues
+    );
 
-    if (resourceOwnerQueryResult.rows.length === 0) return res.status(403).json({ error: 'Unauthorized access to delete this resource'});
+    if (resourceOwnerQueryResult.rows.length === 0)
+      return res
+        .status(403)
+        .json({ error: "Unauthorized access to delete this resource" });
 
     // if owner
-    const deletedResource = await db.query(deleteResourceQueryString, [resource_id]);
+    const deletedResource = await db.query(deleteResourceQueryString, [
+      resource_id,
+    ]);
 
-    res.status(200).json(deletedResource.rows[0])
+    res.status(200).json(deletedResource.rows[0]);
   } catch (error) {
-    console.error('Error deleting resource: ', error);
-    res.status(500).json({ error: 'Internal Server Error'});
+    console.error("Error deleting resource: ", error);
+    res.status(500).json({ error: "Internal Server Error" });
   }
-
 });
 
 module.exports = router;
